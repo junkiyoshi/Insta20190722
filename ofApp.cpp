242a2bb65cf6c382fc9c3302e3dbb5c8c72e30c1
中内 純 <nakauchi.k@g-hits.co.jp>
2019-07-22 19:58:14 +0900

    First

---
+++ ofApp.cpp
@@ -0,0 +1,73 @@
+﻿#include "ofApp.h"	
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+
+	ofSetFrameRate(60);
+	ofSetWindowTitle("openFrameworks");
+
+	ofBackground(239);
+	ofSetColor(39);
+	ofSetLineWidth(2);
+	ofEnableDepthTest();
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+
+	ofSeedRandom(39);
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+
+	ofTranslate(ofGetWidth() * 0.5, ofGetHeight() * 0.5);
+
+	for (auto len = 50; len < ofGetWidth(); len += 50) {
+
+		vector<glm::vec2> vertices;
+		int param_start = ofMap(ofNoise(ofRandom(1000), ofGetFrameNum() * 0.0025), 0, 1, 0, 200);
+		for (auto param = param_start + 5; param <= param_start + 95; param++) {
+
+			vertices.push_back(this->make_point(len, param));
+		}
+
+		ofNoFill();
+		ofBeginShape();
+		ofVertices(vertices);
+		ofEndShape();
+
+		ofFill();
+		ofDrawCircle(this->make_point(len, param_start), 5);
+	}
+
+}
+
+//--------------------------------------------------------------
+glm::vec2 ofApp::make_point(int len, int param) {
+
+	param = param % 100;
+	if (param < 25) {
+
+		return glm::vec2(ofMap(param, 0, 25, -len * 0.5, len * 0.5), -len * 0.5);
+	}
+	else if (param < 50) {
+
+		return glm::vec2(len * 0.5, ofMap(param, 25, 50, -len * 0.5, len * 0.5));
+	}
+	else if (param < 75) {
+
+		return glm::vec2(ofMap(param, 50, 75, len * 0.5, -len * 0.5), len * 0.5);
+	}
+	else {
+
+		return glm::vec2(-len * 0.5, ofMap(param, 75, 100, len * 0.5, -len * 0.5));
+	}
+}
+
+//--------------------------------------------------------------
+int main() {
+
+	ofSetupOpenGL(720, 720, OF_WINDOW);
+	ofRunApp(new ofApp());
+}
